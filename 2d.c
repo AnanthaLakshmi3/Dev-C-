64f35e9a07e168f0dff31f160cf07d021846898e
AnanthaLakshmi3 <durgaananthalakshmigollavilli@gmail.com>
2024-08-15 20:17:21 +0530

    Add files via upload

---
+++ 2d.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int r,c,sum=0;
+	scanf("%d %d",&r,&c);
+	int a[r][c];
+	int i,j;
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+    }
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+			//	printf("%d ",a[i][j]); 
+			sum=sum+a[i][j];
+	     	}
+		printf("%d",sum);
+	    }
+		printf("\n");
+}
