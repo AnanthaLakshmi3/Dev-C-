66e72b9b9d80576d1ca51b59987b4dc33d222536
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-22 20:19:33 +0530

    Add files via upload

---
+++ maxvalue.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int i,a[n];
+	printf("enter elements to array:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	int max=array_max(a,n);
+	printf("%d",max);
+}
+int array_max(int *arr, int m)
+{
+	int i,maxval=arr[0]; 
+	for(i=1;i<m;i++)
+	{
+		if(arr[i]>maxval)
+		{
+			maxval=arr[i];
+		}
+	}
+	return maxval;
+}
