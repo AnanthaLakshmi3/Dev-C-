874149021ab002f7189d23bb8758a2ee80a9f6fc
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-22 20:18:16 +0530

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,22 @@
+//malloc function
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,*p,n;
+	scanf("%d",&n);
+	p=(int*)malloc(n*sizeof(int));
+	if(p==NULL)
+	{
+		printf("memory not allocated");
+	}
+	else
+	{
+		for(i=0;i<n;i++)
+		
+		{
+			scanf("%d",p+i);
+			printf("%u ",*(p+i));
+		}
+	}
+}
