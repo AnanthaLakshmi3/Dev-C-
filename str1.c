0f4e8ac1f6af20cafeefa4846c800f8b6af5a9e8
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-24 19:35:30 +0530

    Add files via upload

---
+++ str1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char str[]="ANUSHA";
+   char rtc[]="AARTHI";
+   char ing[]="anusha";
+ //   printf("%d\n",strlen(str));
+//	printf("%d\n",sizeof(str));
+//	printf("%s\n",strlwr(str));
+//    printf("%s\n",strupr(str));
+//	printf("%s\n",strrev(str));
+ //	printf("%s\n",strcpy(str,rtc));
+  //	printf("%s\n",strcat(str,rtc));
+ - printf("%d\n",(strcmp(str,rtc)));
+   printf("%d\n",(strcmp(str,ing)));
+}
