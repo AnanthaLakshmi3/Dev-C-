19f59f35561a07546d2c92187b0d59d9e4445e87
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-08-25 20:08:04 +0530

    Add files via upload

---
+++ suminintandavginfloat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n=5,total,a,b,c,d,e;
+	float Average;
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	total=(a+b+c+d+e);
+	Average=(total/5);
+	printf("total=%d ",total);
+	printf("Average=%f",Average);
+	
+	
+}
