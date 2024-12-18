c367fb6e70e318a98eba4ba01bbabfccd3ed41bf
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-12-18 22:18:36 +0530

    Add files via upload

--- pointers.c
+++ pointers.c
@@ -1,19 +1,16 @@
-//pointers
-#include<stdio.h>
-int main()
-{
-int a=10,b=9,c;
-int *p,*q;
- p=&a;
- q=&b;
- c=*q;
- *p=20;
- printf("value of a=%d\n",a);
- printf("a=%d\n",*p);
- printf("address of a=%x\n",&a);
- printf("address of a=%x\n",p);
- printf("address of p=%x\n",&p);
- printf("c=%d\n",c);
- printf("a=%d\n",a);
- printf("p=%x",p);
+#include <stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a; 
+    *a = *b;        
+    *b = temp;   
+}
+
+int main() {
+    int num1, num2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    swap(&num1, &num2);
+    printf("%d %d\n", num1, num2);
+
+    return 0;
 }
